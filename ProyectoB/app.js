89743b1b69d5566f00c2b3f46855e15c4c855a5f
Luis-Vergargit config --global user.name Luis-Vergaraa <luiseduardovergarasierra@gmail.com>
2018-05-18 11:56:40 -0500

    FirebaseyBootstrap

--- ProyectoB/app.js
+++ ProyectoB/app.js
@@ -26,3 +26,65 @@ firebase.initializeApp({
             console.error("Error adding document: ", error);
         });
     }
+
+    //Leer documento
+    var tabla = document.getElementById('tabla');
+    db.collection("users").onSnapshot((querySnapshot) => {
+        tabla.innerHTML='';
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+            tabla.innerHTML += `
+            <tr>
+                <th scope="row">${doc.id}</th>
+                <td>${doc.data().first}</td>
+                <td>${doc.data().last}</td>
+                <td>${doc.data().born}</td>
+                <td><button class="btn btn-primary" onclick="editar('${doc.id}','${doc.data().first}','${doc.data().last}','${doc.data().born}')">Editar</button></td>
+                <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
+            </tr>
+            `
+        });
+    });
+
+    //Editar Documento
+    function editar(id,nombre,apellido,fecha){
+        document.getElementById('nombre').value = nombre;
+        document.getElementById('apellido').value = apellido;
+        document.getElementById('fecha').value = fecha;
+        var boton = document.getElementById('boton');
+
+        boton.innerHTML = 'Actualizar';
+
+        boton.onclick = ()=>{
+            var washingtonRef = db.collection("users").doc(id); 
+            var nombre = document.getElementById('nombre').value;
+            var apellido = document.getElementById('apellido').value;
+            var fecha = document.getElementById('fecha').value;      
+
+            return washingtonRef.update({
+                first: nombre,
+                last: apellido,
+                born: fecha
+            })
+            .then(function() {
+                console.log("Document successfully updated!");
+                boton.innerHTML = 'Guardar';
+                document.getElementById('nombre').value='';
+                document.getElementById('apellido').value='';
+                document.getElementById('fecha').value='';
+            })
+            .catch(function(error) {
+                // The document probably doesn't exist.
+                console.error("Error updating document: ", error);
+            });
+            }
+    }
+
+    //Borrar documentos
+    function eliminar(id){
+        db.collection("users").doc(id).delete().then(function() {
+            console.log("Document successfully deleted!");
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+    }
